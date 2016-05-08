698b9f537e45128b9a077cfc482f0c222e2f3e01
Peter Chen <peter@weme.sh>
2016-05-08 16:46:22 -0400

    Remove logging

--- modules/mongo-service.js
+++ modules/mongo-service.js
@@ -7,7 +7,7 @@ var chatlogURL = 'mongodb://localhost:27017/ChatLogs';
 var storeNewMessage = function(roomName, username, msg, callback) {
 	mongo.connect(chatlogURL, function(err, db) {
 		if(err) {
-			console.log(err.toString());
+			console.log(err);
 			return;
 		}
 		var message = {
@@ -30,7 +30,7 @@ exports.storeNewMessage = storeNewMessage;
 var getRecentMessage = function(roomName, callback) {
 	mongo.connect(chatlogURL, function(err, db) {
 		if(err) {
-			console.log(err.toString());
+			console.log(err);
 			return;
 		}
 		var aggregate = [
@@ -38,8 +38,6 @@ var getRecentMessage = function(roomName, callback) {
 			{$limit:10}
 		];
 		db.collection(roomName).aggregate(aggregate, function(err, res) {
-			console.log(err);
-			console.log(res);
 			db.close();
 			if(err) {callback(false, null);}
 			else {callback(true,res.reverse());}
