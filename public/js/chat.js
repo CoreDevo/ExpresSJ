b45a81b2bdc9864d9b0dc420aa675cb797abd2fa
Chuankai Yue <yuechuankai@gmail.com>
2016-05-09 19:39:48 -0400

    chat body always scroll bot (#21)
    * delete node modules
    
    * update gitignore
    
    * fixed redirect issue
    
    * username: msg, pre-QA commit
    
    * missing parameter fixed
    
    * update
    
    * update package.json
    
    * chat UI first commit
    
    * new join user avatar fixed
    
    * username slicing fixed
    
    * chat-body issue fixed
    
    * cookie issue - username
    
    * resolve conflict
    
    * last merge today
    
    * separate css file
    
    * update README
    
    * Merge remote-tracking branch 'CoreDevo/master'
    
    # Conflicts:
    #	public/js/chat.js
    
    * update readme.md
    
    * delete cookie in current domain b4 login
    
    * always scroll to bottom, send button size fixed
    
    * resolve conflict

--- public/js/chat.js
+++ public/js/chat.js
@@ -51,7 +51,7 @@ $(function(){
         //NOTE: This is da fkiing ES6 feature, compatibility should be considered
         // $chat.append('<div class="answer ${direction}"><div class="avatar"><img src="img/avatar-${direction}.jpg" alt="User name"></div><div class="name">${cachedUsername}</div><div class="text">${data.msg}</div><div class="time">1989年年初的时候</div></div>');
         $chat.append('<div class="answer ' +direction+ '"><div class="avatar"><img src="img/avatar-' + direction + '.jpg" alt="User name"></div><div class="name">' + data.username + '</div><div class="text">' + data.msg + '</div><div class="time">Just now</div></div>');
-
+        $('.chat').animate({scrollTop:$('.chat').height()}, 'slow');
     });
 
     socket.on('new join', function(username, roomname, currentNumber){
--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,5 @@
 $(document).ready( function() {
+    deleteAllCookies();
     $('#login-button').click( function() {
         var username=$("#username").val();
 
@@ -19,3 +20,14 @@ $(document).ready( function() {
         return false; // to stop link
     });
 });
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+    	var cookie = cookies[i];
+    	var eqPos = cookie.indexOf("=");
+    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
