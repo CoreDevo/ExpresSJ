be2e19c3c347104e784a71b65a14fc070fd54df0
ckyue <yuechuankai@gmail.com>
2016-05-08 20:54:15 -0400

    chat UI first commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,12 +2,13 @@ $(function(){
     var socket = io.connect();
     var $messageForm = $('#messageForm');
     var $message = $('#message');
-    var $chat = $('#chat');
+    var $chat = $('#chat-body');
     var $roomForm = $('#roomForm');
     var $room = $('#room');
     var roomname = 'lobby';
+    var $onlineUserList = $('#chat-users');
     var cachedUsername = document.cookie;
-    console.log(cachedUsername);
+    var slicedUsername = cachedUsername.slice(16,50)
 
     socket.emit('first connect', roomname, cachedUsername);
 
@@ -20,27 +21,49 @@ $(function(){
 
     $messageForm.submit(function(e){
         e.preventDefault();
-        console.log('working');
+        console.log('message submitted');
         socket.emit('send message', $message.val(), roomname);
         $message.val('');
     });
 
+    socket.on('connect', function(){
+      console.log("first connect")
+      $onlineUserList.append('<div id="' + slicedUsername + '"><div class="user"><div class="avatar"><img src="img/userLIstAvatar.png" alt="User name"></div><div class="name">' + slicedUsername + '</div><div class="user-description">The God</div></div></div>');
+    });
+
     socket.on('new message', function(data){
-        $chat.append('<div class="well">'+ data.username +': ' + data.msg + '</div>');
+      //differentiate sent and received messages
+      //TODO: sent time. eg. 5 mins ago
+      var direction;
+        if(slicedUsername == data.username){
+          //sent by current user
+          direction = "right"
+        }
+        else{
+          direction = "left"
+        }
+        // console.log(direction);
+        //NOTE: This is da fkiing ES6 feature, compatibility should be considered
+        // $chat.append('<div class="answer ${direction}"><div class="avatar"><img src="img/avatar-${direction}.jpg" alt="User name"></div><div class="name">${cachedUsername}</div><div class="text">${data.msg}</div><div class="time">1989年年初的时候</div></div>');
+        $chat.append('<div class="answer ' +direction+ '"><div class="avatar"><img src="img/avatar-' + direction + '.jpg" alt="User name"></div><div class="name">' + slicedUsername + '</div><div class="text">' + data.msg + '</div><div class="time">Just now</div></div>');
+
     });
 
     socket.on('new join', function(username, roomname, currentNumber){
         console.log('In ' + roomname + ', Currently ' + currentNumber);
-        //for testing:
-        $chat.append('<div class="well">' + username + ' joined' + '</div>');
+        //TODO: add number of current user to UI
         $chat.append('<div class="well">Currently ' + currentNumber + '</div>');
+
+        $onlineUserList.append('<div id="' + username + '"><div class="user"><div class="avatar"><img src="img/userLIstAvatar.jpg" alt="User name"></div><div class="name">' + username + '</div></div></div>');
     });
 
     socket.on('new leave', function(username, roomname, currentNumber){
         console.log('In ' + roomname + ', Currently ' + currentNumber);
         //for testing:
-        $chat.append('<div class="well">' + username + ' left' + '</div>');
         $chat.append('<div class="well">Someone left, Currently ' + currentNumber + '</div>');
+
+        //remove offline users on online user list
+        $onlineUserList.remove('.'+username)
     });
 
     socket.on('entered room', function(roomname) {
