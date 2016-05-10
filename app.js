2a3c7b8229c97c3dab67becd79827ab4c18378ee
ckyue <yuechuankai@gmail.com>
2016-05-10 00:38:36 -0400

    code refactored

--- app.js
+++ app.js
@@ -1,158 +1,34 @@
 var express = require('express');
 var app = express();
-var server = require('http').Server(app);
 var index = '/index.html';
 var notFound = '/notFound.html';
-var io = require('socket.io')(server);
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var path = require('path');
-var mongo = require('./modules/mongo-service');
+var logger = require('morgan');
 
-var connections = [];
-var room = ['lobby'];
-var users = [0];
-var roomUsers = {lobby:[]};
+var indexRouter = require('./routers/index');
+var chatRouter = require('./routers/chat');
+var loginRouter = require('./routers/login');
 
+connections = [];
+room = ['lobby'];
+users = [0];
+roomUsers = {lobby:[]};
+
+app.use(logger('dev'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname + '/public')));
 
-app.get('/', function (req, res) {
-  // if (req.cookies.user == null) {
-  //   //no cookie stored, proceed to login page
-  //   res.redirect('/login');
-  // } else {
-  //   //direct cached user to chat room
-  //   res.redirect('/chat');
-  // }
-  res.redirect('/login');
-});
-
-
-//Temp solution for NoName user gettin chat
-//TODO: add friendly alert maybe? NOT NOW!
-/*JUST DONT VISIT CHAT DIRECTLY PLS.
-app.get('/chat', function (req, res) {
-    res.redirect('/login');
-    console.log('chat redirecting to login');
-});*/
-
-app.get('/chat', function (req, res) {
-	res.sendFile(path.resolve('public/chat.html'));
-});
-
-app.get('/login', function (req, res) {
-	console.log("get login");
-	res.sendFile(path.resolve('public/login.html'));
-});
-
-app.post('/login', function (req, res) {
-    var username = req.body.name;
-    console.log("new user name: " + username)
-    //TODO: check if username exists
-    roomUsers['lobby'].push(username);
-    console.log('User in lobby: ' + roomUsers['lobby']);
-    console.log(req.body.name + " cached");
-    res.cookie("userID", req.body.name, {maxAge: 1000*60*60*24*30});
-    res.redirect('/chat');
-});
-
-io.on('connection', function(socket){
-    connections.push(socket);
-    console.log('connected %s', connections.length);
-
-    socket.on('first connect', function(roomname, rawUsername) {
-        //TODO: Temp solotion for cookie username, need better solution
-        var splitArray = rawUsername.split(';');
-        var username = splitArray[splitArray.length-1].split('=')[1];
-        console.log('username is ' + username);
-        socket.join(roomname);
-        socket.room = roomname;
-        socket.user = username;
-        console.log(username + " joined into Room: " + roomname);
-        io.to(roomname).emit('online gods', roomUsers[roomname]);
-        users[0]++;
-    });
-
-    socket.on('enter room', function(roomname){
-        if(roomname == socket.room){
-            socket.emit('new message', { msg: 'You are already in this room'});
-            return;
-        }
-        leaveRoom(socket);
-        if (room.indexOf(roomname) == -1){
-            room.push(roomname);
-            users.push(0);
-            roomUsers[roomname] = [];
-            console.log(room[room.length-1] + ' room: Created');
-        }
-        socket.room = roomname;
-        socket.join(roomname);
-        socket.emit('entered room', roomname);
-        users[room.indexOf(roomname)]++;
-
-        roomUsers[roomname].push(socket.user);
-        io.to(roomname).emit('online gods', roomUsers[roomname]);
-        console.log('User in ' + roomname + ' : ' + roomUsers[roomname]);
-
-        var currentNumber = users[room.indexOf(roomname)];
-        console.log(socket.user + " joined into Room: " + roomname)
-        //TODO: emmit online user list array as well
-        io.to(roomname).emit('new join', socket.user, roomname, currentNumber);
-	    mongo.getRecentMessage(roomname, function(succeed, msgs) {
-		    if(succeed) {
-			    msgs.forEach(function(singleMsg) {
-				    socket.emit('new message', {msg: singleMsg.message, username: singleMsg.username});
-			    });
-		    }
-	    })
-    });
-
-    function leaveRoom(socket){
-        console.log('User is leaving ' + socket.room);
-        var roomname = socket.room;
-        var username = socket.user;
-        socket.leave(socket.room);
-
-        //TODO: Enhance maybe
-        roomUsers[roomname].splice(roomUsers[roomname].indexOf(username), 1);
-        console.log('User in ' + roomname + ' : ' + roomUsers[roomname]);
-
-        var index = room.indexOf(roomname);
-        if (users[index] == 1 && index != 0){
-            users.splice(index, 1);
-            room.splice(index, 1);
-            delete roomUsers[roomname];
-            console.log('Room destroyed');
-        } else {
-            users[index]--;
-            io.to(roomname).emit('online gods', roomUsers[socket.room]);
-            io.to(roomname).emit('new leave', username, roomname, users[index]);
-        }
-    }
-
-    socket.on('send message', function(data, roomname){
-        console.log('Msg: ' + data + ' - in room: ' + roomname + ' - by: ' + socket.user);
-	    mongo.storeNewMessage(roomname, socket.user, data, function(succeed) {
-		    if(succeed) {
-			    io.to(roomname).emit('new message', {msg: data, username: socket.user});
-		    }
-	    })
-    });
-
-    socket.on('disconnect', function(data) {
-        connections.splice(connections.indexOf(socket), 1);
-        console.log('disconnected %s', connections.length);
-    });
-});
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/chat', chatRouter);
 
 app.use(function(req, res){
 	console.log('someone just viewed 404 page');
 	res.sendFile(path.resolve('public/notFound.html'));
 });
 
-server.listen(3000, function(){
-	console.log('Started');
-});
+module.exports = app;
---
+++ modules/socket-service.js
@@ -0,0 +1,100 @@
+/**
+ * Created by JyaouShingan on 2016-05-10.
+ */
+
+var socket_io = require('socket.io');
+var mongo = require('../modules/mongo-service');
+
+var createSocket = function(server) {
+	var io = socket_io(server);
+	io.on('connection', function (socket) {
+		connections.push(socket);
+		console.log('connected %s', connections.length);
+		
+		socket.on('first connect', function (roomname, rawUsername) {
+			//TODO: Temp solotion for cookie username, need better solution
+			var splitArray = rawUsername.split(';');
+			var username = splitArray[splitArray.length - 1].split('=')[1];
+			console.log('username is ' + username);
+			socket.join(roomname);
+			socket.room = roomname;
+			socket.user = username;
+			console.log(username + " joined into Room: " + roomname);
+			io.to(roomname).emit('online gods', roomUsers[roomname]);
+			users[0]++;
+		});
+		
+		socket.on('enter room', function (roomname) {
+			if (roomname == socket.room) {
+				socket.emit('new message', {msg: 'You are already in this room'});
+				return;
+			}
+			leaveRoom(socket);
+			if (room.indexOf(roomname) == -1) {
+				room.push(roomname);
+				users.push(0);
+				roomUsers[roomname] = [];
+				console.log(room[room.length - 1] + ' room: Created');
+			}
+			socket.room = roomname;
+			socket.join(roomname);
+			socket.emit('entered room', roomname);
+			users[room.indexOf(roomname)]++;
+			
+			roomUsers[roomname].push(socket.user);
+			io.to(roomname).emit('online gods', roomUsers[roomname]);
+			console.log('User in ' + roomname + ' : ' + roomUsers[roomname]);
+			
+			var currentNumber = users[room.indexOf(roomname)];
+			console.log(socket.user + " joined into Room: " + roomname)
+			//TODO: emmit online user list array as well
+			io.to(roomname).emit('new join', socket.user, roomname, currentNumber);
+			mongo.getRecentMessage(roomname, function (succeed, msgs) {
+				if (succeed) {
+					msgs.forEach(function (singleMsg) {
+						socket.emit('new message', {msg: singleMsg.message, username: singleMsg.username});
+					});
+				}
+			})
+		});
+		
+		function leaveRoom(socket) {
+			console.log('User is leaving ' + socket.room);
+			var roomname = socket.room;
+			var username = socket.user;
+			socket.leave(socket.room);
+			
+			//TODO: Enhance maybe
+			roomUsers[roomname].splice(roomUsers[roomname].indexOf(username), 1);
+			console.log('User in ' + roomname + ' : ' + roomUsers[roomname]);
+			
+			var index = room.indexOf(roomname);
+			if (users[index] == 1 && index != 0) {
+				users.splice(index, 1);
+				room.splice(index, 1);
+				delete roomUsers[roomname];
+				console.log('Room destroyed');
+			} else {
+				users[index]--;
+				io.to(roomname).emit('online gods', roomUsers[socket.room]);
+				io.to(roomname).emit('new leave', username, roomname, users[index]);
+			}
+		}
+		
+		socket.on('send message', function (data, roomname) {
+			console.log('Msg: ' + data + ' - in room: ' + roomname + ' - by: ' + socket.user);
+			mongo.storeNewMessage(roomname, socket.user, data, function (succeed) {
+				if (succeed) {
+					io.to(roomname).emit('new message', {msg: data, username: socket.user});
+				}
+			})
+		});
+		
+		socket.on('disconnect', function (data) {
+			connections.splice(connections.indexOf(socket), 1);
+			console.log('disconnected %s', connections.length);
+		});
+	});
+};
+
+module.exports = createSocket;
